fe2b19f0638fc7e12a63a8ccc60ff714ec2ec1bc
ieminu <98277683+ieminu@users.noreply.github.com>
2022-01-23 20:50:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+ namespace Console_programlama 
+ {
+     class MainClass
+     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Adını Gir");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Soyadını Gir");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + ad + " " + soyad);
+        }
+     }
+ }
